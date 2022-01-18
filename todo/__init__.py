e32f289b6e5a697b3f757d7c2f0ccc139c992dc3
Yasmin <flor.yasmin90@gmail.com>
2022-01-18 00:46:12 -0300

    adding flask_rest

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
+from flask_restful import Api
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+api = Api()
 
 def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
     db.init_app(app)
     migrate.init_app(app, db)
+    api.init_app(app)
     
     with app.app_context():
         # Import all app models here
