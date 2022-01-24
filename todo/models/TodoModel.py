8a109bc35215663d450642ca253850f92c62d187
Yasmin <flor.yasmin90@gmail.com>
2022-01-24 01:44:20 -0300

    user model

--- todo/models/TodoModel.py
+++ todo/models/TodoModel.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import Model
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql import func
+from passlib.hash import bcrypt
 
 class Todo(Model):
     __tablename__ = "todo"
@@ -13,3 +15,18 @@ class Todo(Model):
 
     def __repr__(self):
         return "<Todo 🞄 {}>"
+
+class User(Model):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(100), unique=True)
+    name = Column(String(200), unique=False, nullable=False)
+    _password = Column(String(100), nullable=False)
+
+    def __repr__(self):
+        return "<User 🞄 {}>"
+    
+    @hybrid_property
+    def password(self, password):
+        hasher = bcrypt.using(rounds=13)
+        return hasher.hash(password)
