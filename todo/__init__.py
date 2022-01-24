fdf8265d9f615853a896cf50e20452a787a5dc3f
Yasmin <flor.yasmin90@gmail.com>
2022-01-24 01:44:06 -0300

    marshmallow

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,19 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_marshmallow import Marshmallow
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 api = Api()
+ma = Marshmallow()
 
 def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+
     db.init_app(app)
     migrate.init_app(app, db)
     api.init_app(app)
-    
+    ma.init_app(app)
+
     with app.app_context():
         
         # Import all app models here
@@ -25,6 +29,6 @@ def init_app():
         app.register_blueprint(todos_bp)
         
         # APIs Resources
-        api.add_resource(TodoResource, '/todo', '/todo/<int:id>', endpoint="todo")
+
         db.create_all()
         return app
