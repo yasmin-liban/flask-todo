4f7217c53448a5f7c7ab8b0e0f2640e0cffbd522
Yasmin <flor.yasmin90@gmail.com>
2022-01-24 22:15:08 -0300

    api fairy config

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    APIFAIRY_TITLE = 'todo'
+    ENV = os.getenv('env', 'development')
